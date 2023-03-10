d76a0a51d746a625fcd5325ae865f57eed432249
jinjinlei2108 <Mofaxvetu1>
2023-03-10 17:31:18 +1100

    modified

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -23,7 +23,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
-        audience: "https://rbm7x5e9gl.execute-api.us-east-1.amazonaws.com/",
+        audience: "https://rbm7x5e9gl.execute-api.us-east-1.amazonaws.com/dev",
         scope: "read:current_user update:current_user_metadata",
       }}
       onRedirectCallback={onRedirectCallback}
