a78e5fadcaaf09171fa3370a84c010713affed5e
jinjinlei2108 <Mofaxvetu1>
2023-03-11 00:41:43 +1100

    test outside context

--- src/Components/EditPhoto.js
+++ src/Components/EditPhoto.js
@@ -5,13 +5,9 @@ import { useArtContext } from "../art_context";
 import { updateItem } from "../Api/ItemApi";
 import { useAuth0 } from "@auth0/auth0-react";
 
-export default async function EditPhoto(itemId) {
+export default function EditPhoto(itemId) {
   const {  getAccessTokenSilently} = useAuth0();
-  const token = await getAccessTokenSilently({
-    authorizationParams: {
-      audience: 'https://rbm7x5e9gl.execute-api.us-east-1.amazonaws.com/dev', // Value in Identifier field for the API being called.
-      scope: 'read:posts', // Scope that exists for the API being called. You can create these through the Auth0 Management API or through the Auth0 Dashboard in the Permissions view of your API.
-    }})
+
 
   const { personalPhotos } = useArtContext();
   const editPhoto = personalPhotos.find((item) => (item.itemId === itemId.itemId));
@@ -41,6 +37,11 @@ export default async function EditPhoto(itemId) {
   };
 
   async function handleSubmit(event) {
+    const token = await getAccessTokenSilently({
+      authorizationParams: {
+        audience: 'https://rbm7x5e9gl.execute-api.us-east-1.amazonaws.com/dev', // Value in Identifier field for the API being called.
+        scope: 'read:posts', // Scope that exists for the API being called. You can create these through the Auth0 Management API or through the Auth0 Dashboard in the Permissions view of your API.
+      }})
     event.preventDefault();
     await updateItem(
       token,
